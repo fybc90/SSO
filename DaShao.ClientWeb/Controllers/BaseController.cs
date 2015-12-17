c7f276b322177e57b0f4b66071e645b67301f1b9
fybc90 <fybc90@163.com>
2015-12-17 16:26:13 +0800

    初始化

---
+++ DaShao.ClientWeb/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaShao.ClientWeb.Controllers
+{
+    public class BaseController : Controller
+    {
+
+    }
+}
